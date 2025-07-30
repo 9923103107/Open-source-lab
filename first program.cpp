b26471c7b242332ef6dc8f37b59aa70948439043
9923103107 <9923103107@mail.jiit.ac.in>
2025-07-30 10:39:57 +0530

    Add files via upload

---
+++ first program.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!" << endl;
+    cout << "This is my first program in C++." << endl;
+    cout << "I am excited to learn more about programming!" << endl;
+    return 0;
+}
